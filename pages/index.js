63e7740146be473a98b1fb6bfc10bf85cc5ade49
Joadson Silva <joadson83@hotmail.com>
2022-01-30 14:19:11 -0300

    included: change state value and ChatPage

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,9 @@
 import appConfig from '../config.json';
+import React from 'react';
+import { useRouter } from 'next/router';
 import { Box, Button, Text, TextField, Image } from '@skynexui/components'; 
 
-function GlobalStyle(){
-    return(
-        <style global jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-      body {
-        font-family: 'Open Sans', sans-serif;
-      }
-      /* App fit Height */ 
-      html, body, #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */ 
-    `}</style>
-    )
-}
+
 function Titulo(props){
     const Tag = props.tag || 'h1';
     return (
@@ -62,11 +37,13 @@ function HomePage() {
 */
 
 export default function PaginaInicial() {
-    const username = 'Joads0n';
+    //const username = 'Joads0n';
+    /* Sempre usar o set para mudança de estado */
+    const [username, setUsername] = React.useState('Joads0n');
+    const roteamento = useRouter();
   
     return (
       <>
-        <GlobalStyle />
         <Box
           styleSheet={{
             display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -93,6 +70,10 @@ export default function PaginaInicial() {
             {/* Formulário */}
             <Box
               as="form"
+              onSubmit = {function(e){
+                e.preventDefault();
+                roteamento.push('/chat')
+              }}
               styleSheet={{
                 display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
                 width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
@@ -104,7 +85,12 @@ export default function PaginaInicial() {
               </Text>
               
               <TextField
+                value = {username}
                 fullWidth
+                onChange = {function (event){
+                  const valor = event.target.value;
+                  setUsername(valor)
+                }}
                 textFieldColors={{
                   neutral: {
                     textColor: appConfig.theme.colors.neutrals[200],
